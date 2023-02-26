98c953f7844eff9008f86014501e610f86e0efac
PageHey <pagehey@pm.me>
2023-02-26 19:54:47 +0100

    update health_check and add subscription base

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,27 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde;
 use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::NoContent()
+async fn health_check() -> HttpResponse {
+    HttpResponse::NoContent().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
-            .route("/{name}", web::get().to(greet))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
